1869b2c303e718fe58946bfd2cc74d6531a2643f
Jason Jiang <ssdog27@gmail.com>
2020-08-26 14:58:38 +0800

    ch1-sharing-setup-ruby-helper

--- ch1/spec/sandwich_spec.rb
+++ ch1/spec/sandwich_spec.rb
@@ -1,20 +1,20 @@
 Sandwich = Struct.new(:taste, :toppings)
 
 RSpec.describe 'An ideal sandwich' do
-  before do 
-    @sandwich = Sandwich.new('delicious', []) 
-  end
-  # do... end = {}
 
+  def sandwich
+    @sandwich ||= Sandwich.new('delicious', [])
+  end
+  
   it 'is delicious' do
-    taste = @sandwich.taste
+    taste = sandwich.taste
 
     expect(taste).to eq('delicious')
   end
 
   it 'let me add toppings' do
-    @sandwich.toppings<<'chese'
-    toppings = @sandwich.toppings
+    sandwich.toppings<<'chese'
+    toppings = sandwich.toppings
 
     expect(toppings).not_to be_empty
   end
