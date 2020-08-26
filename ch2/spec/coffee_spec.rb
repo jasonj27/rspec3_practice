9ebdfb1248ce64373ef0d5865536651ae130f5b5
Jason Jiang <ssdog27@gmail.com>
2020-08-26 15:53:18 +0800

    ch2-focus-specific-example

--- ch2/spec/coffee_spec.rb
+++ ch2/spec/coffee_spec.rb
@@ -14,6 +14,7 @@ end
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = 'spec/examples.txt'
+  config.filter_run_when_matching(focus: true)
 end
 
 RSpec.describe 'A cup of coffee' do
@@ -23,7 +24,7 @@ RSpec.describe 'A cup of coffee' do
     expect(coffee.price).to eq(1.00)
   end
 
-  context 'with milk' do
+  fcontext 'with milk' do #contex 'with milk', focus:true do
     before { coffee.add :milk }
 
     it 'cost $1.25' do
