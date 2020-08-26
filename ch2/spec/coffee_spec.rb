0eef96e13afb4e820990f893e9e0623fa0e86cd9
Jason Jiang <ssdog27@gmail.com>
2020-08-26 16:31:58 +0800

    ch2-completing-WIP

--- ch2/spec/coffee_spec.rb
+++ ch2/spec/coffee_spec.rb
@@ -14,6 +14,14 @@ class Coffee
   def inspect
     "#< #{self.class.name} with #{@ingredients} >".to_s
   end
+
+  def color
+    ingredients.include?(:milk) ? :light : :dark
+  end
+
+  def temperature
+    ingredients.include?(:milk) ? 190.0 : 205.0
+  end
 end
 
 RSpec.configure do |config|
@@ -35,13 +43,13 @@ RSpec.describe 'A cup of coffee' do
       expect(coffee.price).to eq(1.25)
     end
 
-    it 'is light in color' do
-      pending 'Color not implemented yet'
+  xit 'is light in color' do
+      #pending 'Color not implemented yet'
       expect(coffee.color).to be(:light)
     end
 
     it 'is cooler than 200 degree Fahrenheit' do
-      pending 'Temperature not implemented yet'
+      #pending 'Temperature not implemented yet'
       expect(coffee.temperature).to be < 200.0
     end
   end
