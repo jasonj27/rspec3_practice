98d97f9a5c258728c0297dd465309f62361510d6
Jason Jiang <ssdog27@gmail.com>
2020-08-26 14:38:55 +0800

    ch1-sharing-setup-before-hook

--- ch1/spec/sandwich_spec.rb
+++ ch1/spec/sandwich_spec.rb
@@ -1,9 +1,21 @@
 Sandwich = Struct.new(:taste, :toppings)
 
 RSpec.describe 'An ideal sandwich' do
+  before do 
+    @sandwich = Sandwich.new('delicious', []) 
+  end
+  # do... end = {}
+
   it 'is delicious' do
-    sandwich = Sandwich.new('delicious', [])
-    taste = sandwich.taste
+    taste = @sandwich.taste
+
     expect(taste).to eq('delicious')
+  end
+
+  it 'let me add toppings' do
+    @sandwich.toppings<<'chese'
+    toppings = @sandwich.toppings
+
+    expect(toppings).not_to be_empty
   end
 end
