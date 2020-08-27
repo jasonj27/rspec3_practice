536f31c0afc9506ac01cda3c99d1b009c6ee5a9d
Jason Jiang <ssdog27@gmail.com>
2020-08-27 14:12:17 +0800

    finish unit test 'post /expenses'

--- expense_tracker/app/api.rb
+++ expense_tracker/app/api.rb
@@ -11,7 +11,14 @@ module ExpenseTracker
     post '/expenses' do
       expense = JSON.parse(request.body.read) #request.body = {"some"=>"data"}
       result = @ledger.record(expense) #result = #<struct ExpenseTracker::RecordResult :success?=true, expense_id=417, error_message=nil>
-      JSON.generate('expense_id' => result.expense_id)
+
+      if result.success?
+        JSON.generate('expense_id' => result.expense_id)
+      else
+        status 422
+        JSON.generate('error' => result.error_message)
+      end
+
     end
 
     get '/expenses/:date' do
--- expense_tracker/spec/unit/app/api_spec.rb
+++ expense_tracker/spec/unit/app/api_spec.rb
@@ -40,9 +40,27 @@ module ExpenseTracker
       end
 
       context 'when the expense fails validation' do
-        it 'returns an error message'
-        it 'responds with a 422(Unprocessable entity)'
+        let (:expense){{ 'some' => 'data'}}
 
+        before do
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(false, 417, 'Expense incomplete')) #configure test double's behavior
+            #ledger.record({ 'some' => 'data' }) 
+            #return #<struct ExpenseTracker::RecordResult :success?=false, expense_id=417, error_message='Expense incomplete'>
+        end
+
+        it 'returns an error message' do
+          post '/expenses', JSON.generate(expense)
+
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to include('error' => 'Expense incomplete')
+        end
+
+        it 'responds with a 422(Unprocessable entity)' do
+          post '/expenses', JSON.generate(expense)
+          expect(last_response.status).to eq(422)
+        end
       end
     end
   end
