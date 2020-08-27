c151e132343031bf58fe3dd54061893718a3d08d
Jason Jiang <ssdog27@gmail.com>
2020-08-27 13:20:20 +0800

    add test double of Ledger in unit test

--- expense_tracker/spec/unit/app/api_spec.rb
+++ expense_tracker/spec/unit/app/api_spec.rb
@@ -1,10 +1,33 @@
 require_relative '../../../app/api'
+require 'rack/test'
 
 module ExpenseTracker
+  RecordResult = Struct.new(:success?, :expense_id, :error_message)
+
   RSpec.describe API do
-    describe 'POST/expenses' do
-      context 'when the expenses is successfully recorded' do
-        it 'returns the expense id'
+    include Rack::Test::Methods
+
+    def app
+      API.new(ledger: ledger)
+    end
+
+    let(:ledger) { instance_double('ExpenseTracker::Ledger') }
+
+    describe 'POST /expenses' do
+      context 'when the expense is successfully recorded' do
+        it 'returns the expense id' do
+          expense = { 'some' => 'data' }
+
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true, 417, nil)) #configure test double's behavior
+
+          post '/expenses', JSON.generate(expense)
+
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to include('expense_id' => 417)
+        end
+
         it 'responds with a 200(OK)'
 
       end
@@ -12,7 +35,7 @@ module ExpenseTracker
       context 'when the expense fails validation' do
         it 'returns an error message'
         it 'responds with a 422(Unprocessable entity)'
-        
+
       end
     end
   end
