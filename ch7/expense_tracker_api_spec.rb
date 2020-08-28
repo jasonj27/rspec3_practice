c1ccb4def58d0eedcfb319637558466da8962ec8
Jason Jiang <ssdog27@gmail.com>
2020-08-28 16:02:24 +0800

    shared_context

--- ch7/expense_tracker_api_spec.rb
+++ ch7/expense_tracker_api_spec.rb
@@ -1,15 +1,21 @@
-module APIHelpers
+RSpec.shared_context 'API helpers' do
   include Rack::Test::Methods
 
   def app
     ExpenseTracker::API.new
   end
+
+  before do
+    basic_authorize 'test_user', 'test_password'
+  end
 end
 
 RSpec.configure do |config|
-  config.include APIHelpers
+  config.include_context 'API helpers'
 end
+#uses include_context in RSpec.configure (all example groups)
 
 RSpec.describe 'Expense Tracker API', :db do
-
+  include_context 'API helpers'
+  #uses include_context in specific example groups
 end
