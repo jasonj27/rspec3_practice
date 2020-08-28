d7095cc4cd1c50a67f469e4dcdc1d581ef0116de
Jason Jiang <ssdog27@gmail.com>
2020-08-28 15:36:19 +0800

    helper methods (after)

--- ch7/expense_tracker_api_spec.rb
+++ ch7/expense_tracker_api_spec.rb
@@ -1,7 +1,12 @@
-RSpec.describe 'Expense Tracker API', :db do
+module APIHelpers
   include Rack::Test::Methods
 
   def app
     ExpenseTracker::API.new
   end
+end
+
+RSpec.describe 'Expense Tracker API', :db do
+  include APIHelpers
+
 end
