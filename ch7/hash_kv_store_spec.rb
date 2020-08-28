f2a7902a28ece23b3444cbd65526fb3e4ef03f31
Jason Jiang <ssdog27@gmail.com>
2020-08-28 16:24:40 +0800

    shared_examples (it_behaves_like)

--- ch7/hash_kv_store_spec.rb
+++ ch7/hash_kv_store_spec.rb
@@ -1,17 +1,6 @@
 require 'hash_kv_store'
+require 'support/kv_store_shared_examples'
 
 RSpec.describe HashKVStore do
-  let(:kv_store) {HashKVStore.new}
-
-  it 'allows you to fetch previously stored values' do
-    kv_store.store(:language, 'Ruby')
-    kv_store.store(:os, 'linux')
-
-    expect(kv_store.fetch(:language)).to eq 'Ruby'
-    expect(kv_store.fetch(:os)).to eq 'linux'
-  end
-
-  it 'raises a Key Error when you fetch an unknown key' do
-    expect{kv_store.fetch(:foo)}.to raise_error(KeyError)
-  end
+  it_behaves_like 'KV store', HashKVStore
 end
