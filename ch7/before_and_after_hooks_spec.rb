1ddcc09bb359e226cad3fcfc9e4ad114a3cc05b8
Jason Jiang <ssdog27@gmail.com>
2020-08-28 14:52:52 +0800

    scoped example group  contex) hook

--- ch7/before_and_after_hooks_spec.rb
+++ ch7/before_and_after_hooks_spec.rb
@@ -8,3 +8,13 @@ RSpec.describe MyApp::Configuration do
   end
   #guaranteed to run after examples, even if examples fails or before fails
 end
+
+RSpec.describe 'Web interface to my thermostat' do
+  before(:context) do
+    WebBrowser.launch
+  end
+
+  after(:context) do
+    WebBrowser.shutdown
+  end
+end
