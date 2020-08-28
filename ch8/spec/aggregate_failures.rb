60c2d7369efecbd422671aa76476b424892572e1
Jason Jiang <ssdog27@gmail.com>
2020-08-28 18:44:39 +0800

    config aggregate failures

---
+++ ch8/spec/aggregate_failures.rb
@@ -0,0 +1,15 @@
+RSpec.configure do |config|
+  config.define_derived_metadata do |meta|
+    #Sets the flag unconditionally;
+    #doesn't allow examples to opt out
+    meta[:aggregate_failures] = true unless meta.key?(:aggregate_failures)
+  end
+end
+
+RSpec.describe 'Billing', aggregate_failures: false do
+  context 'using the fake payment service' do
+    before do
+      expect(MyApp.config.payment_gateway).to include('sandbox')
+    end
+  end
+end
