837d6bb49721490046579493b811ce4d241dc7f2
Jason Jiang <ssdog27@gmail.com>
2020-08-28 18:35:38 +0800

    derived metadata

---
+++ ch8/spec/spec_helper.rb
@@ -0,0 +1,8 @@
+RSpec.configure do |config|
+  config.define_derived_metadata(file_path:/spec\/unit/) do |meta|
+    meta[:fast] = true
+  end
+
+  config.define_derived_metadata(type: :model) do |meta|
+  end
+end
