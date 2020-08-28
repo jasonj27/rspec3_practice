9d7d289e8f7bf69bd86f82f54c0dcebe69f082b9
Jason Jiang <ssdog27@gmail.com>
2020-08-28 18:57:39 +0800

    filter run filter run when matching

--- ch8/spec/spec_helper.rb
+++ ch8/spec/spec_helper.rb
@@ -8,4 +8,12 @@ RSpec.configure do |config|
 
   config.filter_run_excluding :jruby_only unless RUBY_PLATFORM == 'java'
   #run examples with :jruby_only on jruby platform
+
+  config.filter_run :filter_tag
+  #run only examples with provided tag
+  #it will skip all examples when there's no match example
+
+  config.filter_run_when_matching :filter_tag2
+  #when tag provided, run examples with tag only
+  #run all examples if there's no match example
 end
